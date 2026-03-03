413c430d6a28eba32932ceb3536d67c07ca74726
Crunchcow <Kai.Lemke84@gmail.com>
2026-03-03 10:20:25 +0100

    environment variables for local development, and an example .env file for production. The .env file is not checked into version control, so it can contain sensitive information like the SECRET_KEY without risking exposure. The requirements.txt file has been updated to include python-decouple, which is used to read the environment variables from the .env file.

--- kursanmeldung/settings.py
+++ kursanmeldung/settings.py
@@ -11,21 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rrlka#3jfr=ljx+tfy8=$mda$8=*)vxuo8=%j#jb6!)7-h-8_6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# Sensible Werte aus .env-Datei lesen (niemals direkt im Code!)
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 
 # Application definition
