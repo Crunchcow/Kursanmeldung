96582dedc9f99820ba00aa4b8ab73e7e19389ad5
Crunchcow <Kai.Lemke84@gmail.com>
2026-03-25 16:41:00 +0100

    feat: Microsoft Graph API E-Mail-Backend (App-Registrierung)

--- kursanmeldung/settings.py
+++ kursanmeldung/settings.py
@@ -177,4 +177,11 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@westfalia-osterwick.de')
+
+# Microsoft Graph API (App-Registrierung) für E-Mail-Versand
+MS_TENANT_ID     = config('MS_TENANT_ID', default='')
+MS_CLIENT_ID     = config('MS_CLIENT_ID', default='')
+MS_CLIENT_SECRET = config('MS_CLIENT_SECRET', default='')
+MS_SENDER        = config('MS_SENDER', default='')
+
 SITE_URL = config('SITE_URL', default='http://89.167.0.28')
